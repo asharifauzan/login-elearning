fecac2b509ba59c1c832953e0fdef769e0a48c3f
Fauzan Hibatullah Ashari <52328248+asharifauzan@users.noreply.github.com>
2020-11-22 10:23:14 +0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,19 @@
+let form     = document.getElementsByTagName('form')[0];
+let email    = document.querySelector("[name='email']");
+let password = document.querySelector("[name='password']");
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  // let email    = email.value;
+  // let password = password.value;
+  // console.log(email);
+  // console.log(password);
+  let xhr = new XMLHttpRequest();
+  xhr.open('POST', 'https://asharifauzan.rf.gd/auth/login');
+  xhr.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200) {
+      console.log(this.response);
+    }
+  }
+  xhr.send('email='+email.value+'&password='+password.value);
+});
