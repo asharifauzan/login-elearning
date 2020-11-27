4359f66f227a572b6bdcc9e0f2ab85a8f1804356
Fauzan Hibatullah Ashari <fauzansaja10@gmail.com>
2020-11-27 22:07:16 +0700

    login vps 2

--- login.js
+++ login.js
@@ -10,6 +10,7 @@ form.addEventListener('submit', function(e) {
   // console.log(password);
   let xhr = new XMLHttpRequest();
   xhr.open('POST', 'https://188.166.210.205/elearning/auth/login');
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   xhr.onreadystatechange = function() {
     if(this.readyState == 4 && this.status == 200) {
       console.log(this.response);
